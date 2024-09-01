73b50bfafd49fe0f7239b7e05a6f643745007e8a
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-01 16:12:32 +0700

    Update Fungsionality to Edit Title Section and Style

--- app/services/BodyService.js
+++ app/services/BodyService.js
@@ -6,21 +6,8 @@ class BodyService {
   initEditButtonListener() {
     const title = document.querySelector('.tab-contents__body .section-content__header-title');
     const editTitleButton = document.querySelector('.tab-contents__body .section-content__header__edit-title-button');
-
-    title.addEventListener('blur', () => {
-      title.removeAttribute('contenteditable');
-    });
-  
-    title.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        title.blur();
-      }
-    });
-
-    editTitleButton.addEventListener('click', () => {
-      this.editTitleListener(title);
-    });
+    
+    this.editTitleHandler(title, editTitleButton);
   }
 
   createStepperElement(number) {
@@ -31,49 +18,58 @@ class BodyService {
     return stepper;
   }
 
-  editTitleListener(title) {
-    title.setAttribute('contenteditable', 'true');
+  resetTitle(titleElement, editTitleButton) {
+    titleElement.removeAttribute('contenteditable');
+    editTitleButton.style.display = 'block';
+  }
 
+  editTitleListener(titleElement, editTitleButton) {
+    titleElement.setAttribute('contenteditable', 'true');
+  
+    editTitleButton.style.display = 'none';
+  
     const range = document.createRange();
-    range.selectNodeContents(title);
+    range.selectNodeContents(titleElement);
     range.collapse(false);
     const selection = window.getSelection();
     selection.removeAllRanges();
     selection.addRange(range);
   }
 
-  createBodySectionHeader() {
-    const header = document.createElement('div');
-    const title = document.createElement('h2');
-    const editTitleButton = document.createElement('button');
-    const saveTitleButton = document.createElement('button');
-    const description = document.createElement('p');
-
-    header.classList.add('section-content__header');
-    title.classList.add('section-content__header-title');
-    saveTitleButton.classList.add('section-content__header__edit-title-button');
-    saveTitleButton.classList.add('section-content__header__save-title-button');
-
-    title.innerText = 'Untitled';
-    editTitleButton.innerHTML = '<i class="fa fa-pencil"></i>';
-    saveTitleButton.innerHTML = '<i class="fas fa-save"></i>';
-
-    title.addEventListener('blur', () => {
-      title.removeAttribute('contenteditable');
+  editTitleHandler(title, editTitleButton) {
+    editTitleButton.addEventListener('click', () => {
+      this.editTitleListener(title, editTitleButton);
     });
-  
+
+    title.addEventListener('blur', () =>{
+      this.resetTitle(title, editTitleButton);
+    });
+      
     title.addEventListener('keydown', (event) => {
       if (event.key === 'Enter') {
         event.preventDefault();
         title.blur();
       }
     });
+  }
 
-    editTitleButton.addEventListener('click', () => {
-      this.editTitleListener(title);
-    });
+  createBodySectionHeader() {
+    const header = document.createElement('div');
+    const title = document.createElement('h2');
+    const editTitleButton = document.createElement('button');
+    const description = document.createElement('p');
 
-    header.append(title, editTitleButton, saveTitleButton, description);
+    header.classList.add('section-content__header');
+    title.classList.add('section-content__header-title');
+    editTitleButton.classList.add('section-content__header__edit-title-button');
+
+    title.innerText = 'Untitled';
+    editTitleButton.innerHTML = '<i class="fa fa-pencil"></i>';
+    description.innerText = 'Share details to showcase your qualifications';
+
+    this.editTitleHandler(title, editTitleButton);
+
+    header.append(title, editTitleButton, description);
 
     return header;
   }
@@ -83,16 +79,9 @@ class BodyService {
     form.classList.add('section-content__form');
     
     const header = this.createBodySectionHeader();
-
-    const sectionHtml = `
-      <div class="section-content__body">
-        <input type="text">
-      </div>
-    `;
-
+    
     form.append(header);
 
-    // form.innerHTML = sectionHtml;
     return form;
   }
 }
