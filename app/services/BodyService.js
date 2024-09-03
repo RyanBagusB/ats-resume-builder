4e21c81294bdee7d5e5c2d97833823ea04d5bd78
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-04 01:01:19 +0700

    Add Body Services

--- app/services/BodyService.js
+++ app/services/BodyService.js
@@ -1,59 +1,23 @@
+import { resumeService } from "./resumeService";
+
 class BodyService {
   constructor() {
-    this.initEditButtonListener();
+    this.resume = resumeService.getCV();
+    this.sectionContent = document.querySelector('.tab-contents__body .section-content');
+    this.stepperContainer = document.querySelector('.tab-contents__body .section-navigation__stepper-container');
+    this.bodyElements = [];
   }
 
-  initEditButtonListener() {
-    const title = document.querySelector('.tab-contents__body .section-content__header-title');
-    const editTitleButton = document.querySelector('.tab-contents__body .section-content__header__edit-title-button');
-    
-    this.editTitleHandler(title, editTitleButton);
-  }
-
-  createStepperElement(number) {
+  createStepperElement() {
     const stepper = document.createElement('button');
+    const number = this.bodyElements.length + 1;
     stepper.classList.add('section-navigation__stepper-container__stepper');
     stepper.textContent = number;
 
     return stepper;
   }
 
-  resetTitle(titleElement, editTitleButton) {
-    titleElement.removeAttribute('contenteditable');
-    editTitleButton.style.display = 'block';
-  }
-
-  editTitleListener(titleElement, editTitleButton) {
-    titleElement.setAttribute('contenteditable', 'true');
-  
-    editTitleButton.style.display = 'none';
-  
-    const range = document.createRange();
-    range.selectNodeContents(titleElement);
-    range.collapse(false);
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-
-  editTitleHandler(title, editTitleButton) {
-    editTitleButton.addEventListener('click', () => {
-      this.editTitleListener(title, editTitleButton);
-    });
-
-    title.addEventListener('blur', () =>{
-      this.resetTitle(title, editTitleButton);
-    });
-      
-    title.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        title.blur();
-      }
-    });
-  }
-
-  createBodySectionHeader() {
+  createSectionElement() {
     const header = document.createElement('div');
     const title = document.createElement('h2');
     const editTitleButton = document.createElement('button');
@@ -67,24 +31,46 @@ class BodyService {
     editTitleButton.innerHTML = '<i class="fa fa-pencil"></i>';
     description.innerText = 'Share details to showcase your qualifications';
 
-    this.editTitleHandler(title, editTitleButton);
-
     header.append(title, editTitleButton, description);
 
     return header;
   }
 
-  createBodySection() {
+  createSectionForm() {
     const form = document.createElement('div');
+    const stepperElement = this.createStepperElement();
+    const sectionElement = this.createSectionElement();
+    
     form.classList.add('section-content__form');
-    
-    const header = this.createBodySectionHeader();
-    
-    form.append(header);
+    form.appendChild(sectionElement);
 
-    return form;
+    this.stepperContainer.appendChild(stepperElement);
+    this.sectionContent.appendChild(form);
+
+    const bodyElement = { stepper: stepperElement, form };
+    this.bodyElements.push(bodyElement);
+
+    return bodyElement;
+  }
+
+  save() {
+    console.log(this.bodyElements)
+  }
+
+  load() {
+    const { body } = this.resume;
+
+
+    if (body) {
+      body.forEach((value) => {
+        console.log(value.title);
+      });
+    } else {
+      this.createSectionForm();
+    }
+    
+    return this.bodyElements;
   }
 }
 
 export default new BodyService();
-  
