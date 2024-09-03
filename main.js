1737c10138b291681aba4a85fd47ea61cb011942
Ryan Bagus <ryanbagusbimantoro@gmail.com>
2024-09-04 01:03:48 +0700

    Add Body Controller

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
 import '@fortawesome/fontawesome-free/js/all.min.js';
 import './style.css';
-import ResumeController from './app/controllers/ResumeController';
 import './app/controllers/HeaderController';
-
-const resumeController = new ResumeController();
-resumeController.main();
+import './app/controllers/BodyController';
